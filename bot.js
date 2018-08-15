017bfbdf60a44e8bb7fe3128fd0bbdc51e5bfa3f
IIDARSH <42307827+IIDARSH@users.noreply.github.com>
2018-08-15 19:54:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ LET'S GO!
 client.on('ready', function(){
     client.user.setStatus("dnd");
     var ms = 10000 ;
-    var setGame = [`${PREFIX}help`];
+    var setGame = [`${$}help`];
     var i = -1;
     var j = 0;
     setInterval(function (){
